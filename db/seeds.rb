6472d04edf7f728ef0d26edf6868654ec97af148
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-09 16:37:35 +0900

    wikinimous challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Seeding DB with wonderful entried..."
+10.times do
+  Article.create(title: Faker::Book.title, content: Faker::Quote.matz)
+end
+
+puts "Seeding finished"
